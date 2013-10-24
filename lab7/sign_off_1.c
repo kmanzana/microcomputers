7feca130557e5f89f8ffdec8bb2f04cc08152e2a
Kelton Manzanares <kmanzana@mines.edu>
2013-10-24 13:25:59 -0600

    After completion

--- lab7/sign_off_1.c
+++ lab7/sign_off_1.c
@@ -2,16 +2,17 @@
 #include "derivative.h"
 #include "lcd.h"
 
+
 void main(void) {
   char my_str[] = "Waddup Yo!";
 
-  print_LCD(my_str);
+  initialize_LCD();
 
+  print_LCD(my_str);
+  
   EnableInterrupts;
 
   for(;;) {
     _FEED_COP();
   }
 }
-
-
