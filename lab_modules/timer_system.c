d9b669962c4f1447e723c4514eaacbed0819d7d1
Kelton Manzanares <kmanzana@mines.edu>
2013-10-24 16:57:57 -0600

    Update timer_system.c

--- lab_modules/timer_system.c
+++ lab_modules/timer_system.c
@@ -7,12 +7,17 @@ void setup_timer(char prescaler) {
 
 void setup_output_compare(char channel, char action) {
   TIOS |= channel; // enable OCn function
-  TCTL2 = action;  // set OCn to be no action
+  // TCTL1 = action;  // set OCn to be no action
+  TCTL1 = 0x30; // set OC6 pin action to go high
+  // TCTL2 for other ports
   TFLG1 = channel; // clear the CnF flag
+  TIE |= channel;
 }
 
 void setup_input_capture(char channel, char edge_to_capture) {
+  TIOS &= ~channel;        // enable input-capture
+  TCTL3 = edge_to_capture; // capture rising edges of the PT3 pin
+  // TCTL4 = edge_to_capture; // for other PT ports
   TFLG1 = channel;         // clear the CnF flag
-  TIOS &= ~channel;        // enable input-capture 3
-  TCTL4 = edge_to_capture; // capture rising edges of the PT3 pin
+  TIE |= channel;
 }
