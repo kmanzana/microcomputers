20a62cca5cc84511a48ea2f7af22af225c5c795f
Kelton Manzanares <kmanzana@mines.edu>
2013-11-05 12:19:15 -0700

    added file for project pseudo code

---
+++ project/pseudo/project.rb
@@ -0,0 +1,8 @@
+# Draw a flowchart or write pseudo code for your final project. For large
+# systems, it is better to break up things up into subsystems and draw
+# individual flowcharts or pseudo code for each subsystem. The flowcharts or
+# pseudo code should omit details that are not essential for human
+# understanding, but should be detailed enough that someone can convert it into
+# a program. Include a short paragraph describing your design.
+
+
