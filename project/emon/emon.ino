3bc9de271ca128ffdae043ae688457e3cefe7b5a
Kelton Manzanares <kmanzana@mines.edu>
2013-12-04 08:22:26 -0700

    cleaned up code a bit and got rid of lcd.clear()

--- project/emon/emon.ino
+++ project/emon/emon.ino
@@ -4,63 +4,45 @@
 
 void setupCurrentSensors();
 
-serLCD lcd(1);
-EnergyMonitor emonCT1;                   // Create an instance
+serLCD        lcd(1); // pin to transmit to serLCD backpack (1 = tx)
+EnergyMonitor emonCT1;
 EnergyMonitor emonCT2;
 
 void setup() {
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-
   lcd.setType(5);
   lcd.clear();
   lcd.setBrightness(30);
-
   // setContrast(0x20);
-  // Serial.begin(9600);
+
   setupCurrentSensors();
 }
 
 void loop() {
-  lcd.clear();
   lcd.setCursor(1, 1);
 
-  // measureAndLCDPrintVoltage();
-  measureAndLCDPrintPower();
-  // lcd.print(5.0 * analogRead(A2) / 1024.0);
+  printMeasuredPower();
 
   delay(1000);
 }
 
 void setupCurrentSensors() {
-  emonCT1.current(A1, 57.9);             // Current: input pin, calibration. 60.6
+  emonCT1.current(A1, 57.9); // Current: input pin, calibration; orig = 60.6
   emonCT2.current(A2, 57.9);
 }
 
-void measureAndLCDPrintPower() {
-  double Irms = emonCT1.calcIrms(1480) + emonCT2.calcIrms(1480);  // Calculate Irms only; # of samples
-  double voltage = (emonCT1.readVcc() + emonCT1.readVcc()) / 2;
+void printMeasuredPower() {
+  double irms = emonCT1.calcIrms(1480) + emonCT2.calcIrms(1480);
+  double voltage = emonCT1.readVcc();
 
   lcd.print("EMon: ");
   lcd.print(voltage);
   lcd.print("       ");
-  lcd.print(Irms * 120.0);    // Apparent power
+  lcd.print(irms);          // Irms
   lcd.print("       ");
-  lcd.print(Irms);          // Irms
+  lcd.print(irms * 120.0);    // Apparent power
   lcd.print("       ");
-  lcd.print(analogRead(A1));
-  lcd.print("       ");
-  lcd.print(analogRead(A2));
 
   delay(50);
-}
-
-void measureAndLCDPrintVoltage() {
-  float voltage = emonCT1.readVcc();
-
-  lcd.print("Voltage: ");
-  lcd.print(voltage);
-  lcd.print(" mV");
 }
 
 void setContrast(int contrast) {
